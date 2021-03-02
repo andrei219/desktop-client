53713fcf64419a2db9329a1f3347c6ac03886634
Andrei Enache <andrei.officee@gmail.com>
2021-03-02 14:02:47 +0100

    icons updated and icon.py added

---
+++ icons.py
@@ -0,0 +1,30 @@
+
+
+from tkinter import PhotoImage, Tk
+
+from os import path
+
+
+root = Tk()
+
+add_icon = PhotoImage(file=path.join('icons', 'add.png'))
+enable_partner_icon = PhotoImage(file=path.join('icons', 'enable_partner.png'))
+add_documnent_icon = PhotoImage(file=path.join('icons', 'add_document.png'))
+add_invoice_icon = PhotoImage(file=path.join('icons', 'add_invoice.png'))
+add_payment_icon = PhotoImage(file=path.join('icons', 'add_payment.png'))
+agents_icon = PhotoImage(file=path.join('icons', 'agents.png'))
+assembly_icon = PhotoImage(file=path.join('icons', 'assembly.png'))
+waiting_stock_icon = PhotoImage(file=path.join('icons', 'waiting_stock.png'))
+cancel_icon = PhotoImage(file=path.join('icons', 'cancel.png'))
+check_icon = PhotoImage(file=path.join('icons', 'check.png'))
+courier_icon = PhotoImage(file=path.join('icons', 'courier.png'))
+disable_partner_icon = PhotoImage(file=path.join('icons', 'disable_partner.png'))
+delete_icon = PhotoImage(file=path.join('icons', 'delete.png'))
+edit_icon = PhotoImage(file=path.join('icons', 'edit.png'))
+email_icon = PhotoImage(file=path.join('icons', 'email.png'))
+expense_icon = PhotoImage(file=path.join('icons', 'expense.png'))
+more_options_icon = PhotoImage(file=path.join('icons', 'more_options.png'))
+pdf_icon = PhotoImage(file=path.join('icons', 'pdf.png'))
+print_icon = PhotoImage(file=path.join('icons', 'print.png'))
+unbox_icon = PhotoImage(file=path.join('icons', 'unbox.png'))
+
